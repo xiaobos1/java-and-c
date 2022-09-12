74075a0b3a3d902c8cbf93c2609dbe488f8e23c9
xiaobos1 <3030956356@qq.com>
2022-09-12 19:39:59 +0800

    练习

---
+++ 9_12test/9_12test/lain.cpp
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	char a = 3;
+//	//00000000 000000000 000000000 00000011
+//	//00000011 -a
+//
+//	char b = 127;
+//	//00000000 000000000 000000000 01111111
+//	//01111111
+//		char c = a + b;
+//	//
+//	printf("%d", c);
+//	return 0;
+//}
+//
+
+//int main()
+//{
+//	int a = 4;
+//	float f = 4.5;
+//	a + f;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	int c = a + b * 3;
+//	printf("%d", c);
+//	return 0;
+//}
+
+int main()
+{
+	int i = 0;
+	int ret = (++i) + (++i) = (++i);
+	printf("%d", ret);
+	return 0;
+}
