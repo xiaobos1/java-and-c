d01ae91734ed9499d2ae2409dc58a9f610c55ec6
xiaobos1 <3030956356@qq.com>
2022-09-13 20:18:15 +0800

    类型

---
+++ 9_13test/9_13test/linaxi.cpp
@@ -0,0 +1,70 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	int a = 1;
+//	int* pa = &a;
+//	int arr[] = { 1,2 };
+//	printf("%p\n", pa);
+//	printf("%p",&arr);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int* pa;
+//	char* ch;
+//	float* pc;
+//	printf("%d\n", sizeof(pa));
+//	printf("%d\n", sizeof(ch));
+//	printf("%d\n", sizeof(pc));
+//	return 0;
+//}
+
+//int main()
+//{
+//	//int a = 0x11223344;
+//	////
+//	//int* pa = &a;
+//	//*pa = 0;
+//	//printf("%d",pa);
+//
+	/*int arr[10] = { 0 };
+	int *pa = arr;
+	char *pc = arr;
+	printf("%p\n", pa);
+	printf("%p\n", pa + 1);
+	printf("%p\n", pc);
+	printf("%p\n", pc+1);*/
+
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//    int* pa = arr;
+//	const char* pc = arr;
+//	printf("%p\n", pa);
+//	printf("%p\n", pa + 1);
+//	printf("%p\n", pc);
+//	printf("%p\n", pc + 1);
+//	return 0;
+//	
+//}
+
+
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int* p = arr;
+//	int i = 0;
+//	for (i = 1; i < 10; i++)
+//	{
+//		p += i;
+//		printf("%d\n", p);
+//	}
+//	return 0;
+//}
+
