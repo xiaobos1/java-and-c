6d829410a0ddb9a17b9065d92caaa32770fe298a
hebo <3030956356@qq.com>
2022-09-16 20:01:10 +0800

    测试

---
+++ 9_16test/9_16test/lina.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int arr[10] = { 0 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		arr[i] = i + 1;
+
+	}
+	return 0;
+}
