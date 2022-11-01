6ffcbad748d5bc9ee4dda0ff623fb730421d6b8a
hebo <3030956356@qq.com>
2022-11-01 22:48:06 +0800

    练习

---
+++ Test—11/Project1/源.cpp
@@ -0,0 +1,79 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//有1、2、3、4个数字，能组成多少个互不相同且无重复数字的三位数？都是多少？
+
+//int main()
+//{
+//	int i, j, k;
+//	int count = 0;
+//	int arr[n];
+//	//i表示百位的1-4
+//	for (i = 1; i <= 4; i++) 
+//	{
+//		for (j = 1; j <= 4; j++)//j表示十位的范围1-4
+//		{
+//			for (k = 1; k <= 4; k++)//k表示个位
+//			{
+//				if (i != j && i != k && j != k)//因为每一位不能重复，
+//				{
+//					arr[count] = i * 100 + j * 10 + k;
+//					count++;//表示数组的个数也就是每一个三位数的个数
+//				}
+//			}
+//		}
+//	}
+//	int num = count;
+//	printf("有%d个不重复的个数\n", num);
+//	for (i = 0; i < count; i++) {
+//		printf("%d ", arr[i]);
+//	}
+//	
+//	return 0;
+//}
+
+//int main()
+//{
+//	int j, i;	
+//	for (i = 100; i <=200; i++)
+//	{
+//		int num = 0;
+//		for (j = 2; j <i; j++)
+//		{
+//			if (i % j == 0)
+//			{
+//				break;
+//			}
+//		}
+//		if (i == j) {
+//			printf("%d ", i);
+//		}
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int money = 0;
+//	double num = 0;
+//	scanf("%d", &money);
+//	if (money <= 100000)
+//	{
+//		num = money * 0.1;
+//	}
+//	else if (money < 200000)
+//	{
+//		num = 100000 * 0.1 + (money - 10) * 0.075;
+//	}
+//	else if (money <= 400000)
+//	{
+//		num = (money - 200000) * 0.05;
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
+int main()
+{
+	printf("狗娃");
+	return 0;
+}
